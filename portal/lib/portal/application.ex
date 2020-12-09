63919c1e78efc06a022f8e4a4b089dca43620a4a
Alokit Jindal <iamalokit@gmail.com>
2020-12-10 00:31:35 +0530

    Portal Application

--- portal/lib/portal/application.ex
+++ portal/lib/portal/application.ex
@@ -8,13 +8,14 @@ defmodule Portal.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      {DynamicSupervisor, strategy: :one_for_one, name: Portal.DoorSupervisor}
       # Starts a worker by calling: Portal.Worker.start_link(arg)
       # {Portal.Worker, arg}
     ]
 
+
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Portal.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link(children, strategy: :one_for_one, name: Portal.Supervisor)
   end
 end
