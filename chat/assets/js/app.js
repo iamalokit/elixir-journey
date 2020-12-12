fcbf4da16f3d424a7d254a18939af5aadb1bad44
Alokit Jindal <iamalokit@gmail.com>
2020-12-12 16:18:06 +0530

    working chatting app

--- chat/assets/js/app.js
+++ chat/assets/js/app.js
@@ -10,6 +10,29 @@ import "../css/app.scss"
 // Import deps with the dep name or local files with a relative path, for example:
 //
 //     import {Socket} from "phoenix"
-//     import socket from "./socket"
+    import socket from "./socket"
+    let channel = socket.channel('room:lobby', {});
+    channel.on('shout', function (payload) {
+        let li = document.createElement("li");
+        let name = payload.name || 'guest';
+        li.innerHTML = '<b>' + name + '</b>: '+ payload.message;
+        ul.appendChild(li);
+    });
+
+    channel.join();
+
+    let ul = document.getElementById('msg-list');
+    let name = document.getElementById('name');
+    let msg = document.getElementById('msg');
+
+    msg.addEventListener('keypress', function (event) {
+        if(event.keyCode == 13 && msg.value.length > 0){
+            channel.push('shout', {
+                name: name.value, 
+                message: msg.value
+            });
+            msg.value = '';
+        }
+    });
 //
 import "phoenix_html"
--- chat/lib/chat_web/channels/room_channel.ex
+++ chat/lib/chat_web/channels/room_channel.ex
@@ -4,6 +4,7 @@ defmodule ChatWeb.RoomChannel do
   @impl true
   def join("room:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -21,12 +22,22 @@ defmodule ChatWeb.RoomChannel do
   # broadcast to everyone in the current topic (room:lobby).
   @impl true
   def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+    Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert
+    broadcast(socket, "shout", payload)
     {:noreply, socket}
   end
 
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
+  end
+
+  def handle_info(:after_join, socket) do
+    Chat.Message.get_messages()
+    |> Enum.each(fn msg -> push(socket, "shout", %{
+      name: msg.name,
+      message: msg.message,
+    }) end)
+    {:noreply, socket}
   end
 end
