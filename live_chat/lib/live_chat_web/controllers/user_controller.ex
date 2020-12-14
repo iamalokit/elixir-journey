e225e14dbeb4ca559459f7495594fd06247ba8f2
Alokit Jindal <iamalokit@gmail.com>
2020-12-14 19:49:10 +0530

    Adding show.html.eex index page , fix update hash pwd

--- live_chat/lib/live_chat_web/controllers/user_controller.ex
+++ live_chat/lib/live_chat_web/controllers/user_controller.ex
@@ -30,4 +30,34 @@ defmodule LiveChatWeb.UserController do
         render(conn, "new.html", changeset: changeset)
     end
   end
+
+  def edit(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    changeset = User.changeset(user)
+    render(conn, "edit.html", user: user, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Repo.get!(User, id)
+    changeset = User.reg_changeset(user, user_params)
+
+    case Repo.update(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User updated successfully.")
+        |> redirect(to: Routes.user_path(conn, :show, user))
+        {:error, changeset} ->
+          render(conn, "edit.html", user: user, changeset: changeset)
+    end
+  end
+
+
+  def delete(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    Repo.delete!(user)
+
+    conn
+    |> put_flash(:danger, "User deleted successfully.")
+    |> redirect(to: Routes.user_path(conn, :index))
+  end
 end
