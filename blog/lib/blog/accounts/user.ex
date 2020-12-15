1e9db24c375f0f06f4feb1c44eea00d1f86af4cd
Alokit Jindal <iamalokit@gmail.com>
2020-12-16 00:08:20 +0530

    Created the user schema

---
+++ blog/lib/blog/accounts/user.ex
@@ -0,0 +1,39 @@
+defmodule Blog.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :username])
+    |> validate_required([:name, :username])
+    |> unique_constraint(:username)
+  end
+
+
+  def registration_changeset(struct, params) do
+    struct
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset(valid?: true, changes: %{password: pass}) ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+        _ ->
+          changeset
+    end
+  end
+end
---
+++ blog/lib/blog_web/controllers/controller_helpers.ex
@@ -0,0 +1,21 @@
+defmodule BlogWeb.ControllerHelpers do
+  @moduledoc """
+  Provides miscellanious post functions used by multiple modules
+  """
+
+  alias BlogWeb.Auth
+
+  @doc """
+  Returns a list of blog posts.
+
+  if the user is logged in , unpublished posts are included. Otherwise only published posts are included.
+  """
+
+  def list_authorized_posts(conn) do
+    if Auth.logged_in?(conn) do
+      Blog.Posts.list_posts()
+    else
+      Blog.Posts.list_published_posts()
+    end
+  end
+end
