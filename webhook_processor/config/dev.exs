51118e4a71e0c12a77317a51096e952880f12847
Alokit Jindal <iamalokit@gmail.com>
2020-12-17 18:09:58 +0530

    adding tests for webhooks

--- webhook_processor/config/dev.exs
+++ webhook_processor/config/dev.exs
@@ -1,3 +1,3 @@
 use Mix.Config
 
-config :webhook_processor, port:4001
+config :webhook_processor, port: 4001
--- webhook_processor/test/webhook_processor_test.exs
+++ webhook_processor/test/webhook_processor_test.exs
@@ -1,8 +1,36 @@
-defmodule WebhookProcessorTest do
-  use ExUnit.Case
+defmodule WebhookProcessor.EndpointTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  @opts WebhookProcessor.Endpoint.init([])
   doctest WebhookProcessor
 
-  test "greets the world" do
-    assert WebhookProcessor.hello() == :world
+  test "it returns pong" do
+    conn = conn(:get, "/ping")
+    conn = WebhookProcessor.Endpoint.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "pong!"
   end
+
+  test "it returns 200 with a valid payload" do
+    conn = conn(:post, "/events", %{events: [%{}]})
+    conn = WebhookProcessor.Endpoint.call(conn, @opts)
+    assert conn.status == 200
+  end
+
+  test "it returns 422 with an invalid payload" do
+    conn = conn(:post, "/events", %{})
+
+    conn = WebhookProcessor.Endpoint.call(conn, @opts)
+    assert conn.status == 422
+  end
+
+  test "it returns 404 when no route matches" do
+    conn = conn(:get, "/fail")
+    conn = WebhookProcessor.Endpoint.call(conn, @opts)
+    assert conn.status == 404
+  end
+
 end
