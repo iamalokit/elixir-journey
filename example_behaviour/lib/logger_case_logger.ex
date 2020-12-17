0ffb2c3e5fcfa458f8aaf1b10443dd952c84fd4a
Alokit Jindal <iamalokit@gmail.com>
2020-12-17 12:15:39 +0530

    creating a behaviour and their implementations

---
+++ example_behaviour/lib/logger_case_logger.ex
@@ -0,0 +1,17 @@
+defmodule ExampleBehaviour.LowerCaseLogger do
+  @behaviour ExampleBehaviour.Logger
+
+  alias ExampleBehaviour.Logger
+
+  @impl Logger
+  @spec log_message(Logger.log_list(), Logger.level(), String.t()) :: Logger.log_list()
+  def log_message(log_list, level, message) do
+    [{level, String.downcase(message) | log_list}]
+  end
+
+  @impl Logger
+  @spec log_data(Logger.log_list(), Logger.level(), any()) :: Logger.log_list()
+  def log_data(log_list, level, data) do
+    [{level, inspect(data)} | log_list]
+  end
+end
---
+++ example_behaviour/lib/upper_case_logger.ex
@@ -0,0 +1,18 @@
+defmodule ExampleBehaviour.UpperCaseLogger do
+  @behaviour ExampleBehaviour.Logger
+
+  alias ExampleBehaviour.Logger
+
+  @impl Logger
+  @spec log_message(Logger.log_list(), Logger.level(), String.t()) :: Logger.log_list()
+  def log_message(log_list, level, message) do
+    [{level, String.upcase(message) | log_list}]
+  end
+
+  @impl Logger
+  @spec log_data(Logger.log_list(), Logger.level(), any()) :: Logger.log_list()
+  def log_message(log_list, level, data) do
+    # inspect method creates the string representation of any data
+    [{level, inspect(data) | log_list}]
+  end
+end
